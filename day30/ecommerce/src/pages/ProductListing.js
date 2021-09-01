8483150f34a1ea6b4d3070e670b31a1904de44c4
Aditya Pandey <85360307+Adityacmd@users.noreply.github.com>
2021-09-01 17:17:10 +0530

    Add files via upload

---
+++ day30/ecommerce/src/pages/ProductListing.js
@@ -0,0 +1,12 @@
+
+function ProductListing(){
+    // const items = useSelector(state => state.ProductListing.item)
+    return(
+        <div className="items-container">
+            
+        </div>
+    );
+
+}
+
+export default ProductListing;
